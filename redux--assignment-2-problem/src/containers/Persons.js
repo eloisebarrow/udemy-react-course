cdcb5876e56f8c44245cc85f131236b5ce77b22f
eloisebarrow <eloisebarrow@gmail.com>
2019-11-16 17:00:59 -0500

    completed assignment 2 from redux

---
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from '../store/actions';
+
+class Persons extends Component {
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.personAdded} />
+                {this.props.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.personDeleted(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        persons: state.ppl.persons
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        personAdded: () => dispatch({ type: actionTypes.PERSON_ADDED }),
+        personDeleted: (personId) => dispatch({ type: actionTypes.PERSON_DELETED, value: personId })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
